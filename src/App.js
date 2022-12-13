30493abbfb96e4d557db56f89cce55e7df8c4018
Mibek Shrestha <mibekstha@gmail.com>
2022-12-13 13:03:24 +0545

    Header file completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
+import { BiHome } from 'react-icons/bi';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
+import Home from './components/Home';
+
 function App() {
   return <Router>
     <Header />
     <Routes>
 
-      <Route />
+      <Route path='/' element={<Home />} />
     </Routes>
   </Router>
 }
